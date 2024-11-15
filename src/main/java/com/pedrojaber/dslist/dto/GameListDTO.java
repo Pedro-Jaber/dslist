5142ecac7cb9aa79b57a6eafb8269b1cce35b9cc
Pedro Jaber <pedrojaberc@gmail.com>
2024-11-15 19:03:51 -0300

    feat: GET lists

---
+++ src/main/java/com/pedrojaber/dslist/dto/GameListDTO.java
@@ -0,0 +1,26 @@
+package com.pedrojaber.dslist.dto;
+
+import com.pedrojaber.dslist.entities.GameList;
+
+public class GameListDTO {
+
+    private Long id;
+    private String name;
+
+    public GameListDTO() {
+    }
+
+    public GameListDTO(GameList gameList) {
+        this.id = gameList.getId();
+        this.name = gameList.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
